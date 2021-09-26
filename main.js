1ec2973e09405307e0b39db599287e2be73d1f20
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2021-09-26 12:45:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL("BirthdayImage.jpg", function(Img){
+        imageobject = Img;
+
+        imageobject.scaleToWidth(700);
+        imageobject.scaleToHeight(510);
+        imageobject.set({
+            top:0,
+            left:0
+        });
+        canvas.add(imageobject);
+    });
+	
+};
+
+function play(){
+	document.getElementById("myAudio").play();
+}
